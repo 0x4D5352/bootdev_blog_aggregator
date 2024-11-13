f170c9aecfc957541be16bbc7f17d783f1d9e779
Joshua Nussbaum <67082011+joshuanussbaum@users.noreply.github.com>
2024-11-13 16:30:29 -0600

    basic config implementation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/0x4D5352/bootdev_blog_aggregator
+
+go 1.23.3
---
+++ internal/config/config.go
@@ -0,0 +1,60 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+const configFileName = ".gatorconfig.json"
+
+type Config struct {
+	DbUrl           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+func Read() Config {
+	gatorPath, err := getConfigFilePath()
+	gatorHome, err := os.ReadFile(gatorPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var config Config
+	json.Unmarshal(gatorHome, &config)
+	return config
+}
+
+func getConfigFilePath() (string, error) {
+	userHome, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	gatorPath := fmt.Sprintf("%s/%s", userHome, configFileName)
+	return gatorPath, nil
+}
+
+func (cfg *Config) SetUser(username string) {
+	cfg.CurrentUserName = username
+	err := write(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func write(cfg *Config) error {
+	contents, err := json.Marshal(cfg)
+	if err != nil {
+		return err
+	}
+	gatorPath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(gatorPath, contents, 0666)
+	if err != nil {
+		return err
+	}
+	return nil
+}
